7bcd2a6d548631e45846463bab65383c51efb9c6
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-10-02 12:45:58 +1000

    ShellCheck warnings fixed in .sh script

--- project/compile.sh
+++ project/compile.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC2207
+
 shopt -s extglob
 
 target="release"
@@ -7,18 +9,18 @@ target="release"
 [[ -d $target ]] && rm -rf "$target"
 mkdir "$target"
 
-common="/unsafe \
-/optimize \
-/platform:anycpu \
-/target:exe \
-/out:\"release/deoldify.exe\" \
-/r:System.Drawing.dll \
-/r:System.Windows.Forms.dll"
+common=(/unsafe
+    /optimize
+    /platform:anycpu
+    /target:exe \
+    /out:release/deoldify.exe
+    /r:System.Drawing.dll
+    /r:System.Windows.Forms.dll)
 
-src="$(echo src/*.cs)"
+src=($(echo src/*.cs))
 
-resources="$(echo src/resources/*.@(jpg|png|hmodel) | tr ' ' '\n' | sed -E 's/^/\/resource:/')"
+resources=($(echo src/resources/*.@(jpg|png|hmodel) | tr ' ' '\n' | sed -E 's/^/\/resource:/'))
 
-csc $common $resources $src
+csc "${common[@]}" "${resources[@]}" "${src[@]}"
 
 read -rn 1
